2df60cef26faeb08ceba6ac317d6693123c44f66
GabuniyaHub <dianka.gabuniya@gmail.com>
2026-07-27 17:22:13 +0500

    Delete old scripts and add new files of client

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import http from 'node:http';
+
+const server = http.createServer( (request, response) => {
+
+
+}).listen(4080, localhost, () => {
+    console.log("The server start listening on port 4080.")
+});
+
